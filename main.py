ecdc8ecf5c5898c9e66ab9f9b6dbcebdcd7a5cc1
Turtle Code Youtube <85156399+turtlecode@users.noreply.github.com>
2025-03-21 19:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ def delete_file(filename):
         os.remove(file_path)
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
